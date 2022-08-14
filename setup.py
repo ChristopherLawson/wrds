5872923264c5b1892830075914c605c07502590d
Christopher Lawson <chris@lawson.net.au>
2022-08-13 20:40:41 -0500

    bump minor version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = [
 
 setup(
     name='wrds',
-    version='3.1.2',
+    version='3.1.3',
     description="Python access to WRDS Data",
     long_description=open('README.rst').read(),
     author='WRDS',
